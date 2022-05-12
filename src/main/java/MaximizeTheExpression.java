13276bcdc1d45be513f09bd2f18779a889b4ade1
Gourav Sharma <sgourav533@gmail.com>
2022-05-12 23:15:46 +0530

    Arrays-1

---
+++ src/main/java/MaximizeTheExpression.java
@@ -0,0 +1,60 @@
+public class MaximizeTheExpression {
+
+    // int Arr[N]
+    //int p,q,r;
+    //maximize the expression Max(p*Arr[i] + q*Arr[j] + r*Arr[k]) such that i < j < k
+
+    //if the question would be Max(Arr[i] + Arr[j] + Arr[k]) such that i < j < k --> then we could have solve it by fixing the middle elem (i.e 1 to N-1)
+    // and maintain the prefixMax and suffixMax array and loop through the middle elem and find the max via Max(ans , x)
+    // where x = pMax[j-1] + a[j] + sMax[j+1] and ans is initialized to Int.min_value
+
+    //same for this question we can calculate the Pmax array my multiplying each elem with p and sMax with r and compare the ans var....
+    public static void main (String [] args){
+
+        int arr[] =  {1,2,3,4,-5};
+        int p = 1, q= 2, r = -3;
+        System.out.println(byBruteForceMethod(arr, p,q,r));
+
+        //using pMax and sMax
+        int arr1[] = {1,2,3,4,-5};
+        int[] prefixMaxSumArray = getPrefixMaxSumArray(arr1, p);
+        int[] suffixMaxSumArray = getSuffixMaxSumArray(arr1, r);
+        int ans = Integer.MIN_VALUE;
+        for (int j = 1; j<arr1.length-1; j++){
+            ans = Math.max(ans, prefixMaxSumArray[j-1] + r*arr1[j] + suffixMaxSumArray[j+1]);
+        }
+        System.out.println(ans);
+
+    }
+
+    // o( n^3 )
+    private static int byBruteForceMethod(int arr[], int p, int q,int r){
+        int ans = Integer.MIN_VALUE;
+        for (int i = 0; i<arr.length; i++){
+            for (int j = i+1; j<arr.length; j++){
+                for (int k = j+1; k<arr.length; k++){
+                    ans = Math.max(ans, (p*arr[i] + q*arr[j] + r*arr[k]));
+                }
+            }
+        }
+        return ans;
+    }
+
+    private static int[] getPrefixMaxSumArray(int [] arr1, int p){
+        int[] arr = arr1;
+        arr [0] = arr[0] * p;
+        for (int i=1; i< arr.length; i++){
+            arr[i] = Math.max(arr[i-1] , p*arr[i]);
+        }
+        return arr;
+    }
+
+    private static int[] getSuffixMaxSumArray(int [] arr1, int r){
+        int[] arr = arr1;
+        arr[arr.length-1] = r * arr[arr.length-1];
+        for (int i= arr.length-2; i>=0; i--){
+            arr[i] = Math.max(arr[i+1], r*arr[i]);
+        }
+        return arr;
+    }
+}
---
+++ src/main/java/PreComputationTechnique.java
@@ -0,0 +1,81 @@
+import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class PreComputationTechnique {
+
+
+    public static void main(String[] args){
+        // In the given problem, there is an integer array ‘Arr[N]’ and we have to print the individual sum of ‘Q’ subarrays whose
+        // first index - ‘l’ and the last index - ‘r’ is given
+
+        int [] arr = {1,2,3,4,5};
+        String Q = "1,2 1,3 1,4 0,0";
+        System.out.println(LocalDateTime.now());
+        //using bruteForce
+        Arrays.stream(Q.split(" ")).forEach(q -> {
+            List<String> collect = Arrays.stream(q.split(",")).collect(Collectors.toList());
+            long l = sumOfQueryBruteForce(arr, Integer.parseInt(collect.get(0)), Integer.parseInt(collect.get(1)));
+
+            System.out.println( "by brute force "+ q + " --> " + l);
+        } );
+
+        System.out.println(LocalDateTime.now());
+        // using prefixSum
+        int[] prefixSumArray = getPrefixMaxSumArray(arr);
+        Arrays.stream(Q.split(" ")).forEach(q -> {
+            List<String> collect = Arrays.stream(q.split(",")).collect(Collectors.toList());
+            long l = 0l;
+            if (Integer.parseInt(collect.get(0)) == 0){
+                l = prefixSumArray[Integer.parseInt(collect.get(1))];
+            }
+            else {
+                l = prefixSumArray[Integer.parseInt(collect.get(1))] - prefixSumArray[Integer.parseInt(collect.get(0)) - 1];
+            }
+
+            System.out.println( "by prefix max sum "+ q + " --> " + l);
+        } );
+        System.out.println(LocalDateTime.now());
+
+        // In the given problem, there is an integer array ‘Arr[N]’ and we have to print the individual sum of ‘Q’ subarrays whose
+        // int idx -> max elem from 0 .... to idx
+        int [] arr1 = {5, -1, 0, 4, 2, 7};
+        String idx = "0 1 2 3 4";
+
+        System.out.println(LocalDateTime.now());
+        // using prefixMax
+        int[] prefixMaxElementArray = getPrefixMaxElementArray(arr1);
+        Arrays.stream(idx.split(" ")).forEach(q -> {
+            System.out.println( "by prefix max Element "+ q + " --> " + prefixMaxElementArray[Integer.parseInt(q)]);
+        } );
+        System.out.println(LocalDateTime.now());
+    }
+
+//    Create two nested loops to print the sum of different subarrays respectively.
+//    Time Complexity: O(Q*N) --> since will we called for q queries
+//    Space Complexity: O(1)
+    private static long sumOfQueryBruteForce(int[] arr, int l, int r) {
+        long sum = 0;
+        for (int i = l; i <= r; i++){
+            sum += arr[i];
+        }
+        return sum;
+    }
+
+    private static int[] getPrefixMaxSumArray(int [] arr){
+        for (int i=1; i<arr.length; i++){
+            arr[i] += arr[i-1];
+        }
+        return arr;
+    }
+
+    private static int[] getPrefixMaxElementArray(int [] arr){
+
+        for (int i=1; i< arr.length; i++){
+            //System.out.println(Math.max(arr[i-1] , arr[i]));
+            arr[i] = Math.max(arr[i-1] , arr[i]);
+        }
+        return arr;
+    }
+}
