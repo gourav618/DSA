884cefa207194e50d846d17e256c2c5058843d26
Gourav Sharma <sgourav533@gmail.com>
2022-05-22 22:05:00 +0530

    array rearranging alternative technique

--- src/main/java/Arrays/ArrayRearrangement.java
+++ src/main/java/Arrays/ArrayRearrangement.java
@@ -22,13 +22,44 @@ public class ArrayRearrangement {
         // note we can even have more than 1 cycle in an array so we may have to track visited item and not visit them again
         // this can be done by keeping a boolean array , but again we use extra o(N) space
         //so instead we can use some marker like moving elem x would be -x and -ve sign indicates this is visited
-        //edge 0 would fail since there is no -ve 0 , so instead we can do -(x+1) and move the item in ccle and later on convert them back by multiplying -1 and then subtract -1
+        //edge 0 would fail since there is no -ve 0 , so instead we can do -(x+1) and move the item in cycle and later on convert them back by multiplying -1 and then subtract -1
         int []arr1 = {2,0,1,4,5,3};
-        arrangeArrayByCycleMethod(arr1);
+        arrangeArrayByCycleMethod(arr1.clone());
+        System.out.println();
+
+        //alternative technique
+        //ex we have arr[N]->{__,6,___5,_} , here for i=2 we want to assign arr[6] = 2 and arr[6] has 5 in it which indicate arr[5] need to be assign 6 and so on..
+        //THE IDEA is we want to replace arr[6] with a num which represent old_val and new_val both , where old_val=5 and new_val=2 and both lies in 0<=x<=N-1
+        //what should be done for num ?? assume we have 2 int (a,b) < N both less than N , then
+        // we can establish an equation num = N*a + b where a-> Quotient, b-> Remainder, num-> Dividend and N-> Divisor AND num%N = b AND num/N = a
+        //hence, num = N* new_val + old_val and we replace arr[6] = num i.e 8*2 + 5 = 21 old_val = 21%8 = 5 and new_val = 21/8 = 2
+        // each index in  array is replace with num
+        //NOTE: num = N* old_val + new_val -> does this equation make sense No. because while we traverse arr from left to
+        // right to get the old value we do arr[j]%N and if the index is not modified (i.e if y) then y%n = y always since 0<=y<=N-1 , so it make sense to keep the old value as remainder
+        // since new value is set as arr[old_val] = N*j + arr[old_val]
+        // ex: {2 0 1 4 5 3} N=6 -> at j= 0 arr[j] = 2 so we need to make arr[2] as 6*0 + 1 where old_val = 1, new_val = 0 and arr[j] = 6*1 + 2 where old_val = 2, new_val = 1
+        arrangeArrayByAlternativeMethod(arr1.clone());
 
     }
 
-    // 2*n operation i.e time -> o(N)
+    // time -> o(N) and space -> o(1)
+    private static void arrangeArrayByAlternativeMethod(int[] arr) {
+
+        int N = arr.length;
+        // update each index with value num = N* new_val + old_val
+        for (int i = 0; i< N; i++){
+            int old_val = arr[i]% N;
+            arr[old_val] = N * i + arr[old_val];
+        }
+        //update each index with new value
+        for (int i = 0; i< N; i++){
+            arr[i] = arr[i]/ N;
+        }
+
+        Arrays.stream(arr).forEach(System.out::print);
+    }
+
+    // 2*n operation i.e time -> o(N) and space -> o(1)
     private static void arrangeArrayByCycleMethod(int[] arr) {
 
         for (int i=0; i<arr.length; i++){
