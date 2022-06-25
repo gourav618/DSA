ab93b68abcb1b4da8805628dce1c4a3beec91fc8
Gourav Sharma <gourav.sharma@mitlabs.io>
2022-06-25 22:40:16 +0530

    SubSet of a Set using BitMasking

--- src/main/java/Recursion/Concept/SubsetOfASubset.java
+++ src/main/java/Recursion/Concept/SubsetOfASubset.java
@@ -19,6 +19,44 @@ public class SubsetOfASubset {
 
         printAllSubSet(arr, arr.length, 0, tmp, 0);
 
+        System.out.println();
+
+//        Bitmasking is a technique used to select a certain number of bits from a collection of bits.
+//        we will learn how to find all the subsets of a set with the help of bitmasking.
+//        binary representation of digits from 0 to 2^N-1 accounts for all the possible subsets of a set S with ‘N’ elements.
+//        Therefore, we can iterate over the numbers to find their binary representation and extract the 1s to print the elements of the subset.
+//        Time complexity: O(2^N)
+//        Space complexity: O(N)
+        printAllSubSetUsingBitMasking(arr, arr.length);
+
+    }
+
+    private static void printAllSubSetUsingBitMasking(int[] arr, int N) {
+        for (int i=0; i<Math.pow(2, N); i++){
+            // Function to print integer to binary using
+            // inbuilt toBinaryString method
+            //System.out.println(Integer.toBinaryString(num));
+            int[] binary = generateBinaryRepresentation(i, N);
+
+            System.out.print("{ ");
+            for (int k=0; k< binary.length; k++){
+                if (binary[k] == 1) {
+                    System.out.print(arr[k] + " ");
+                }
+            }
+            System.out.print("} ");
+        }
+    }
+
+    private static int[] generateBinaryRepresentation(int i, int N) {
+        int[] binary = new int[N];
+
+        int id = 0;
+        while (i > 0){
+            binary[id++] = i%2;
+            i = i/2;
+        }
+        return binary;
     }
 
     private static void printAllSubSet(int[] arr, int N, int i, int[] tmp, int size) {
