1adfdd0c75510517e7342f41dcddf1dc544dc803
Gourav Sharma <sgourav533@gmail.com>
2023-05-30 09:53:53 +0530

    index of extra elements

--- src/main/java/Searching/Questions/IndexOfExtraElement.java
+++ src/main/java/Searching/Questions/IndexOfExtraElement.java
@@ -1,9 +1,6 @@
 package Searching.Questions;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class IndexOfExtraElement {
