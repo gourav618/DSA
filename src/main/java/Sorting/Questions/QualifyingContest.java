541f5f862250e05c4c38a288d0283fc4d462e357
Gourav Sharma <sgourav533@gmail.com>
2023-06-26 10:36:18 +0530

    optimized qualifying contest question to o(nlogn + m)

--- src/main/java/Sorting/Questions/QualifyingContest.java
+++ src/main/java/Sorting/Questions/QualifyingContest.java
@@ -9,18 +9,20 @@ public class QualifyingContest {
     public static void main(String[] args){
         //https://codeforces.com/contest/659/problem/B
 
-        //todo : optimise this code current time complexity is O(n*m) where n is number of participants and m is number of regions
+
         Scanner s = new Scanner(System.in);
         int n = s.nextInt();
         int m = s.nextInt();
         s.nextLine();
         List<Triplet> participants = new ArrayList<>();
+        int[] studentsPerRegion = new int[m];
         for (int i=0; i<n; i++){
             Triplet t = new Triplet();
             String[] s1 = Arrays.stream(s.nextLine().split(" ")).toArray(String[]::new);
             t.name = s1[0];
             t.region = Integer.parseInt(s1[1]);
             t.score = Integer.parseInt(s1[2]);
+            studentsPerRegion[t.region-1]++;
             participants.add(t);
         }
 
@@ -32,11 +34,45 @@ public class QualifyingContest {
             }
         });
 
-        List<List<String>> teams = new ArrayList<>();
+        int numberOfStudentsInPrevRegion = 0;
         for (int i=0; i<m; i++){
+            int totalStudentInRegion = studentsPerRegion[i];
+            if (i==0){
+                if (totalStudentInRegion > 2 && participants.get(1).score == participants.get(2).score){
+                    System.out.println("?");
+                }else {
+                    if (totalStudentInRegion >= 2) {
+                        System.out.println(participants.get(0).name + " " + participants.get(1).name);
+                    }else {
+                        System.out.println("?");
+                    }
+                }
+            }else {
+                if (totalStudentInRegion > 2 && participants.get(numberOfStudentsInPrevRegion+1).score
+                        == participants.get(numberOfStudentsInPrevRegion+2).score) {
+                    System.out.println("?");
+                }else {
+                    if (totalStudentInRegion >= 2) {
+                        System.out.println(participants.get(numberOfStudentsInPrevRegion).name + " " + participants.get(numberOfStudentsInPrevRegion+1).name);
+                    }else {
+                        System.out.println("?");
+                    }
+                }
+            }
+            numberOfStudentsInPrevRegion += totalStudentInRegion;
+        }
+
+
+        //(n*m) time complexity
+        //printQualifyingTeams(n, m, participants);
+    }
+
+    private static void printQualifyingTeams(int n, int m, List<Triplet> participants) {
+        List<List<String>> teams = new ArrayList<>();
+        for (int i = 0; i< m; i++){
             int count = 0, prev = -1;
             List<String> t = new ArrayList<>();
-            for (int j=0; j<n; j++){
+            for (int j = 0; j< n; j++){
                 if (count < 2 && prev != participants.get(j).score && participants.get(j).region == i+1){
                     t.add(participants.get(j).name);
                     prev = participants.get(j).score;
