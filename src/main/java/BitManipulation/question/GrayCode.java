dbea0bd5e856db071988f4c8322b43cb2c0166e5
Gourav Sharma <sgourav533@gmail.com>
2023-09-22 10:55:19 +0530

    bit manipulation : generate gray code - anothewr way

--- src/main/java/BitManipulation/question/GrayCode.java
+++ src/main/java/BitManipulation/question/GrayCode.java
@@ -13,7 +13,7 @@ public class GrayCode {
     }
 
     private static List<Integer> grayCode(int n) {
-        List<String> grayCode = getGrayCode(n);
+        List<String> grayCode = getGrayCode1(n);
         List<Integer> res = new ArrayList<>();
         for (String l : grayCode){
             res.add(Integer.parseInt(l, 2));
@@ -45,5 +45,25 @@ public class GrayCode {
             idx++;
         }
         return res;
+    }
+
+    private static List<String> getGrayCode1(int n) {
+        if (n ==1){
+            List<String> baseResult = new ArrayList<>();
+            baseResult.add("0");
+            baseResult.add("1");
+            return baseResult;
+        }
+        List<String> res = getGrayCode1(n - 1);
+        List<String> mRes = new ArrayList<>();
+        for (int i=0; i< res.size(); i++){
+            String rStr = res.get(i);
+            mRes.add("0"+rStr);
+        }
+        for (int i=res.size()-1; i >= 0; i--){
+            String rStr = res.get(i);
+            mRes.add("1"+rStr);
+        }
+        return mRes;
     }
 }
